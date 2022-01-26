0c154d3268caab02219833ef40a225131d93f136
EFFICIENT CORPORATES <93851682+EFFICIENTCORPORATES@users.noreply.github.com>
2022-01-26 16:38:21 +0530

    Added version 1.2
    One new package added and some unimportant packages removed

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ README=(HERE/"README.md").read_text()
 
 setup(
 	name = 'effcorp-filetools',
-	version = '1.1.0',
+	version = '1.2.0',
 	py_modules = ['effcorp-filetools'],
 	packages=["filetools"],
 	include_package_data=True,
 	author = 'Efficient_Corporates',
 	author_email = 'efficientcorporates.info@gmail.com',
-	install_requires=['pandas','numpy','openpyxl','XlsxWriter','reportlab','PyPDF2','pdfminer','datetime','tabula','tabulate','PyPDF4','pdf2image','python-pptx'],
-	url = 'https://github.com/EFFICIENTCORPORATES/efffcorp-filetools',
+	install_requires=['pandas','numpy','openpyxl','XlsxWriter','reportlab','PyPDF2','pdfminer','datetime','tabula','tabulate','PyPDF4','fpdf'],
+	url = 'https://github.com/EFFICIENTCORPORATES/effcorp-filetools',
 	description = 'A python module to simplify various files related work in your day to day office or personal work',
 	long_description=README,
 	long_description_content_type="text/markdown",
